fa554da24554f50f08ac6de5a0bac53f06a16cdf
KremnevViacheslav <rusted2192@gmail.com>
2022-04-13 21:48:59 +0300

    Initial Commit

---
+++ TrafficLightV2/TrafficLightV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightV2App.swift
+//  TrafficLightV2
+//
+//  Created by Вячеслав Кремнев on 13.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
